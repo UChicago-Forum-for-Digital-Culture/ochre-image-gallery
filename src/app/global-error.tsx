47f666312ab7e5fd9013fc0494df46ac5dfeecda
Firat Ciftci <ciftcifirat1@gmail.com>
2024-06-26 18:15:06 -0400

    Stylistic updates, bug fixes, enhanced functionality

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -7,7 +7,7 @@ export default function GlobalErrorPage() {
   useRedirect();
 
   return (
-    <div className="absolute bottom-0 left-0 right-0 top-0 grid content-center justify-items-center gap-1.5 text-center font-sans text-xl font-semibold md:mt-20">
+    <div className="absolute bottom-0 left-0 right-0 top-0 grid content-center justify-items-center gap-1.5 px-2 text-center font-sans text-xl font-semibold md:mt-20">
       <TriangleAlertIcon className="-mt-4 h-14 w-auto" />
       Something went wrong, please try again later.
     </div>
